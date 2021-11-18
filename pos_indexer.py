d7c7fc7e114209654a688a3b8bfff9759ed2b9ec
bang-a-coder <nikitas4717@gmail.com>
2021-11-18 23:13:16 +0100

    positional indexer

--- pos_indexer.py
+++ pos_indexer.py
@@ -5,19 +5,20 @@ docs = {
 	1: 'new home sales top forecasts new',
 	2: 'home sales rise in july',
 	3: 'increase in home sales in july',
-	4: 'july new home sales rise',
-	5: 'breakthrough drug for schizophrenia',
-	6: 'new schizophrenia drug',
-	7: 'new approach for treatment of schizophrenia new hopes for schizophrenia patients',
+	4: 'july new home sales rise new',
+	5: 'breakthrough drug for schizophrenia new',
+	6: 'new schizophrenia drug new',
+	7: 'new approach for new treatment of schizophrenia new hopes for schizophrenia patients',
 	8: 'The inventor Stanford Ovshinsky never went to university',
 	9: 'Jonathan went to Stanford University last year'
 }
 
 def pos_indexer(dic):
-	split_dic = {}
+	split_dic = defaultdict(set)
 
 	for key, text in dic.items():
 		split_dic[key] = normalizer(tokenizer(text))
+
 
 	pos_index = defaultdict(set)
 
@@ -25,10 +26,11 @@ def pos_indexer(dic):
 		for token in tokens:
 			pos_index[token] = defaultdict(set)
 
-			for key, tokens in split_dic.items():
-				return
+			for key2, takens in split_dic.items():
+				pos_index[token][key2] = [index for index in range(
+    									len(takens)) if takens[index] == token]
 
 
-	return split_dic
+	return pos_index
 
-print(pos_indexer(docs))
+print(pos_indexer(docs)['new'])
--- test.py
+++ test.py
@@ -7,4 +7,6 @@ lame['new'] = defaultdict(set)
 
 lame['new'][1] = [4,4,2]
 lame['new'][2] = [2,4,5]
-print(lame['new'])
+print(lame)
+
+# {1: ['new', 'home', 'sale', 'top', 'forecast', 'new'], 2: ['home', 'sale', 'rise', 'in', 'july'], 3: ['increase', 'in', 'home', 'sale', 'in', 'july'], 4: ['july', 'new', 'home', 'sale', 'rise'], 5: ['breakthrough', 'drug', 'for', 'schizophrenia'], 6: ['new', 'schizophrenia', 'drug'], 7: ['new', 'approach', 'for', 'treatment', 'of', 'schizophrenia', 'new', 'hope', 'for', 'schizophrenia', 'patient'], 8: ['the', 'inventor', 'stanford', 'ovshinsky', 'never', 'went', 'to', 'university'], 9: ['jonathan', 'went', 'to', 'stanford', 'university', 'last', 'year']}
